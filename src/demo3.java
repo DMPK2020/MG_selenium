603e1c870043477e7b1c6392f52798a8525de190
hp <hp@192.168.2.4>
2020-02-26 14:11:52 +0530

    done

--- src/demo3.java
+++ src/demo3.java
@@ -3,9 +3,6 @@ public class demo3 {
 public static void main(String[] args) {
 	System.out.println(123);
 	System.out.println(586);
-	System.out.println(123);
-	System.out.println(586);
-	System.out.println(123);
-	System.out.println(586);
+
 }
 }
